39b99bb903967731150c138d98dd939ff50f86a3
ppayter-cc <pomsarpeter@gmail.com>
2016-05-11 14:19:58 +0200

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -4,37 +4,46 @@ import time
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 from random import randint
 
-win = curses.initscr()
-#win = curses.newwin(3, 3, 0, 0)
-win.keypad(1)
-curses.noecho()
-curses.curs_set(0)
-#win.border(0)
-win.nodelay(1)
+
+
+screen = curses.initscr()
+#bo = curses.initscr()
+dims = screen.getmaxyx()
+screen = curses.newwin(int(dims[0]), int(dims[1]),0 ,0)
+screen.box()
+for i in range (0, int(dims[0])):
+    screen.addstr(i, int(dims[1]/3), '|')
+    screen.addstr(i, int(dims[1]/3*2), '|')
+for i in range (0, int(dims[1])):
+    screen.addstr(int(dims[0]/3), i, '_')
+    screen.addstr(int(dims[0]/3*2), i, '_')
+    #screen.addstr(i, int(dims[1]), '|')
+    #screen = curses.newwin(int(dims[0]/3),int(dims[1]/3),int(dims[0]-dims[0]/3) ,int(dims[1]-dims[1]/3))
 
 curses.noecho()
 curses.cbreak()
-win.keypad(True)
+screen.keypad(True)
+
 while True:
-    c = win.getch()
+    c = screen.getch()
     if c == ord('7'):
-        win.addstr(0,0,"x")
+        screen.addstr(0,0,"x")
     elif c == ord('8'):
-        win.addstr(0,1,"x")
+        screen.addstr(0,1,"x")
     elif c == ord('9'):
-        win.addstr(0,2,"x")
+        screen.addstr(0,2,"x")
     elif c == ord('4'):
-        win.addstr(1,0,"x")
+        screen.addstr(1,0,"x")
     elif c == ord('5'):
-        win.addstr(1,1,"x")
+        screen.addstr(1,1,"x")
     elif c == ord('6'):
-        win.addstr(1,2,"x")
+        screen.addstr(1,2,"x")
     elif c == ord('1'):
-        win.addstr(2,0,"x")
+        screen.addstr(2,0,"x")
     elif c == ord('2'):
-        win.addstr(2,1,"x")
+        screen.addstr(2,1,"x")
     elif c == ord('3'):
-        win.addstr(2,2,"x")
+        screen.addstr(2,2,"x")
     elif c == ord('q'):
         break  # Exit the while loop
 
