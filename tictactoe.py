d80deb31c7ef1dfc11918e7e87a204ed35c81d7c
ppayter-cc <pomsarpeter@gmail.com>
2016-05-11 14:49:15 +0200

    Add files via upload
    prints 'x' in the middle of the squares

--- tictactoe.py
+++ tictactoe.py
@@ -23,27 +23,30 @@ for i in range (0, int(dims[1])):
 curses.noecho()
 curses.cbreak()
 screen.keypad(True)
+curses.curs_set(0)
+
+# print ("Use the number keys to place the symbols or press 'q' to quit.")
 
 while True:
     c = screen.getch()
     if c == ord('7'):
-        screen.addstr(0,0,"x")
+        screen.addstr(int(dims[0]/6),int(dims[1]/6),"x")
     elif c == ord('8'):
-        screen.addstr(0,1,"x")
+        screen.addstr(int(dims[0]/6),int(dims[1]/2),"x")
     elif c == ord('9'):
-        screen.addstr(0,2,"x")
+        screen.addstr(int(dims[0]/6),int(dims[1]/6*5),"x")
     elif c == ord('4'):
-        screen.addstr(1,0,"x")
+        screen.addstr(int(dims[0]/6*3),int(dims[1]/6),"x")
     elif c == ord('5'):
-        screen.addstr(1,1,"x")
+        screen.addstr(int(dims[0]/6*3),int(dims[1]/2),"x")
     elif c == ord('6'):
-        screen.addstr(1,2,"x")
+        screen.addstr(int(dims[0]/6*3),int(dims[1]/6*5),"x")
     elif c == ord('1'):
-        screen.addstr(2,0,"x")
+        screen.addstr(int(dims[0]/6*5),int(dims[1]/6),"x")
     elif c == ord('2'):
-        screen.addstr(2,1,"x")
+        screen.addstr(int(dims[0]/6*5),int(dims[1]/2),"x")
     elif c == ord('3'):
-        screen.addstr(2,2,"x")
+        screen.addstr(int(dims[0]/6*5),int(dims[1]/6*5),"x")
     elif c == ord('q'):
         break  # Exit the while loop
 
