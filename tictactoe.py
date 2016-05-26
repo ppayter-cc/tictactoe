ccf0c6aaf937d46ce564507a44297f7a23d68ce7
ppayter-cc <pomsarpeter@gmail.com>
2016-05-26 10:18:18 +0200

    reconfigured gameboard

--- tictactoe.py
+++ tictactoe.py
@@ -13,9 +13,9 @@ z = 0  # for the 'test' function
 def current_player():
     global z
     if z == 0:
-        return screen.addstr(2, 1, playernames[0] + " is next!" + " "*5)
+        return screen.addstr(4, 10, playernames[0] + " is next!" + " "*30)  # whitespaces to overwrite unneccessary characters if a name is longer than the other
     elif z == 1:
-        return screen.addstr(2, 1, playernames[1] + " is next!" + " "*5)
+        return screen.addstr(4, 10, playernames[1] + " is next!" + " "*30)
 
 
 def test(i):  # alternates between 'X' and 'O'
@@ -42,52 +42,52 @@ def rewrite(i):  # to avoid overwriting existing steps
         write = False
 
 
-def getPlayerMove(c):  # prints the players' symbols on the space associated with the num keys
+def get_player_move(c):  # prints the players' symbols on the space associated with the num keys
     if c == ord('7'):
         i = 7
         rewrite(i)
         if write:
-            screen.addstr(int(dims[0]/6), int(dims[1]/6), test(i))
+            screen.addstr(int(dims[0]/8*3), int(dims[1]/6), test(i))
     elif c == ord('8'):
         i = 8
         rewrite(i)
         if write:
-            screen.addstr(int(dims[0]/6), int(dims[1]/2), test(i))
+            screen.addstr(int(dims[0]/8*3), int(dims[1]/2), test(i))
     elif c == ord('9'):
         i = 9
         rewrite(i)
         if write:
-            screen.addstr(int(dims[0]/6), int(dims[1]/6*5), test(i))
+            screen.addstr(int(dims[0]/8*3), int(dims[1]/6*5), test(i))
     elif c == ord('4'):
         i = 4
         rewrite(i)
         if write:
-            screen.addstr(int(dims[0]/6*3), int(dims[1]/6), test(i))
+            screen.addstr(int(dims[0]/8*5), int(dims[1]/6), test(i))
     elif c == ord('5'):
         i = 5
         rewrite(i)
         if write:
-            screen.addstr(int(dims[0]/6*3), int(dims[1]/2), test(i))
+            screen.addstr(int(dims[0]/8*5), int(dims[1]/2), test(i))
     elif c == ord('6'):
         i = 6
         rewrite(i)
         if write:
-            screen.addstr(int(dims[0]/6*3), int(dims[1]/6*5), test(i))
+            screen.addstr(int(dims[0]/8*5), int(dims[1]/6*5), test(i))
     elif c == ord('1'):
         i = 1
         rewrite(i)
         if write:
-            screen.addstr(int(dims[0]/6*5), int(dims[1]/6), test(i))
+            screen.addstr(int(dims[0]/8*7), int(dims[1]/6), test(i))
     elif c == ord('2'):
         i = 2
         rewrite(i)
         if write:
-            screen.addstr(int(dims[0]/6*5), int(dims[1]/2), test(i))
+            screen.addstr(int(dims[0]/8*7), int(dims[1]/2), test(i))
     elif c == ord('3'):
         i = 3
         rewrite(i)
         if write:
-            screen.addstr(int(dims[0]/6*5), int(dims[1]/6*5), test(i))
+            screen.addstr(int(dims[0]/8*7), int(dims[1]/6*5), test(i))
     return()
 
 
@@ -119,9 +119,9 @@ def winner2():  # returns true if player2 wins
 
 def winning():  # whether one of the players won, prints this text
     if winner1():
-        screen.addstr(int(dims[0]/2-2), int(dims[1]/2), playernames[0] + " won!")
+        screen.addstr(4, 10, playernames[0] + " won!" + " "*30)
     elif winner2():
-        screen.addstr(int(dims[0]/2-2), int(dims[1]/2), playernames[1] + " won!")
+        screen.addstr(4, 10, playernames[1] + " won!" + " "*30)
     return()
 
 
@@ -129,13 +129,15 @@ screen = curses.initscr()  # draws the board
 dims = screen.getmaxyx()
 screen = curses.newwin(int(dims[0]), int(dims[1]), 0, 0)
 screen.box()
-for i in range(0, int(dims[0])):
+for i in range(int(dims[0]/4), int(dims[0])):
     screen.addstr(i, int(dims[1]/3), '|')  # vertical
     screen.addstr(i, int(dims[1]/3*2), '|')
 for i in range(0, int(dims[1])):
-    screen.addstr(int(dims[0]/3), i, '_')  # horizontal
-    screen.addstr(int(dims[0]/3*2), i, '_')
-screen.addstr(0, 1, "Use number keys to place 'X' and 'O', press 'q' to quit or press 'r' to restart!")
+    screen.addstr(int(dims[0]/4), i, '_')
+    screen.addstr(int(dims[0]/2), i, '_')  # horizontal
+    screen.addstr(int(dims[0]/4*3), i, '_')
+screen.addstr(0, 0, "Use the numpad keys to place 'X' and 'O', press 'q' to quit or press 'r' to restart!")
+screen.addstr(4, 10, playernames[0] + " is next!" + " "*30)
 
 
 curses.noecho()
@@ -147,6 +149,7 @@ while True:
     if winner1() or winner2():  # if one of the players won
         winning()
         board = ['-'] * 10  # fills the board list to prevent more moves
+        z = 5
 
     c = screen.getch()
 
@@ -157,17 +160,18 @@ while True:
         dims = screen.getmaxyx()
         screen = curses.newwin(int(dims[0]), int(dims[1]), 0, 0)
         screen.box()
-        for i in range(0, int(dims[0])):
+        for i in range(int(dims[0]/4), int(dims[0])):
             screen.addstr(i, int(dims[1]/3), '|')  # vertical
             screen.addstr(i, int(dims[1]/3*2), '|')
         for i in range(0, int(dims[1])):
-            screen.addstr(int(dims[0]/3), i, '_')  # horizontal
-            screen.addstr(int(dims[0]/3*2), i, '_')
-        screen.addstr(0, 1, "Use number keys to place 'X' and 'O', press 'q' to quit or press 'r' to restart!")
+            screen.addstr(int(dims[0]/4), i, '_')
+            screen.addstr(int(dims[0]/2), i, '_')  # horizontal
+            screen.addstr(int(dims[0]/8*6), i, '_')
+        screen.addstr(0, 0, "Use number keys to place 'X' and 'O', press 'q' to quit or press 'r' to restart!")
         board = [' '] * 10
         z = 0
     elif c != ord('q'):  # if 'q' is not pressed the game continues
-        getPlayerMove(c)
+        get_player_move(c)
         current_player()
 
 
